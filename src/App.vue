4acda459037690155f4de4eb08edb2f3bcbc1443
denys_slobodianyk_centric <dslobodianyk.e@centricsoftware.com>
2026-04-29 12:21:04 +0300

    fix pipeline build

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,11 @@
     </v-main>
 
     <Footer />
-
   </v-app>
 </template>
 
 <script lang="ts" setup>
   import HelloWorld from '@/components/HelloWorld.vue'
-  import Header from '@/layout/Header.vue'
   import Footer from '@/layout/Footer.vue'
+  import Header from '@/layout/Header.vue'
 </script>
