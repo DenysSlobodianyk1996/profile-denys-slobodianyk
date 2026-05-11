7c02fa9d8eeef67a100df8eb8ff9aec6dc5ab7d4
denys_slobodianyk_centric <dslobodianyk.e@centricsoftware.com>
2026-05-11 13:27:48 +0300

    feat: track isBottomSheetOpened, loadUsedLibs on open

--- src/layout/navigation/InfoAction.vue
+++ src/layout/navigation/InfoAction.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-bottom-sheet>
+  <v-bottom-sheet v-model="isBottomSheetOpened">
     <template #activator="{props: activatorProps}">
       <v-list-item
         v-tooltip:right="t('action.info')"
@@ -25,28 +25,35 @@
 
 </template>
 
-<script setup>
+<script setup lang="ts">
   import { storeToRefs } from 'pinia'
-  import { computed, onMounted } from 'vue'
+  import { computed, ref, watch } from 'vue'
   import { useI18n } from 'vue-i18n'
   import { dataService } from '@/services'
   import { useDataStore } from '@/stores'
 
   const { t } = useI18n()
-
   const dataStore = useDataStore()
 
   const { usedLibs } = storeToRefs(dataStore)
+  const isBottomSheetOpened = ref(false)
   const cardTitle = computed(() => `${t('createdWith')}:`)
 
   async function loadUsedLibs () {
-    const usedLibs = await dataService.getUsedLibs()
-    dataStore.setUsedLibs(usedLibs)
+    if (usedLibs.value.length > 0) {
+      // Don't load again if we already have the data
+      return
+    }
+    const usedLibsResponse = await dataService.getUsedLibs()
+    dataStore.setUsedLibs(usedLibsResponse)
   }
 
-  onMounted(() => {
-    loadUsedLibs()
-  })
+  watch(
+    () => isBottomSheetOpened.value,
+    (newIsBottomSheetOpened: boolean) => {
+      newIsBottomSheetOpened && loadUsedLibs()
+    },
+  )
 </script>
 
 <style lang="scss" scoped>
